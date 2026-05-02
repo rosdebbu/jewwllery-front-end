a933f9bde9058849d243051e1a571be4154453d9
debjitttdasss <rossdebb452@gmail.com>
2026-05-03 01:46:11 +0530

    login server

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
 
+const GOOGLE_CLIENT_ID = "334978160494-aicrlo8m07kdpejtv420e79ddfp1qrvu.apps.googleusercontent.com";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
